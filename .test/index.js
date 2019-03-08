98acd5e7e202cba334d7ea888c757a89182c1e8d
jauntywunderkind <jaunty+wunder+kind+git@eldergods.com>
2019-03-08 03:17:54 -0500

    Add .test/map.js

---
+++ .test/index.js
@@ -0,0 +1,2 @@
+import { * as map } from "./map.js"
+import ( * as foreach } from "./foreach.js"
---
+++ .test/map.js
@@ -0,0 +1,22 @@
+import tape from "tape"
+
+import map from "../async-map.js"
+
+tape( "async map 1", async function( t){
+	t.plan(6)
+	const expected= [ 2, 4, 6, 8]
+	async function *generator(){
+		yield 1
+		yield 2
+		yield 3
+		yield 4
+	}
+	const mapped= map(generator(), x=> x* 2)
+	t.equal( expected.length, 4, "expected is full")
+	for await (const x of mapped){
+		const expecting= expected.shift()
+		t.equal( x, expecting, `expect number ${expecting}`)
+	}
+	t.equal( expected.length, 0, "expected is empty")
+	t.end()
+})
